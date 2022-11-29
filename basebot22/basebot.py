250e537e3bad3be6e9ee3ec0a36f1423f1c2d4af
Justin Guese <guese.justin@gmail.com>
2022-11-29 12:23:53 +0100

    earnings routes

--- basebot22/basebot.py
+++ basebot22/basebot.py
@@ -127,10 +127,22 @@ class BaseBot:
         # raise NotImplementedError("getDecision not implemented")
         return randint(-1, 1)
     
-    def getEarnings(self, only_now = True):
-        response = get(self.backendurl + '/data/earnings-calendar?now=%s' % str(only_now).lower() , headers=self.headers)
+    def getEarningsCalendar(self, only_now: bool = True):
+        response = get(self.backendurl + '/data/earnings/calendar?now=%s' % str(only_now).lower() , headers=self.headers)
         if response.status_code != 200:
             raise Exception("Error getting current earnings: ", response.text)
+        return response.json()
+    
+    def getEarningsFinancials(self, ticker: str, only_now: bool = True):
+        response = get(self.backendurl + '/data/earnings/financials?ticker=%s&now=%s' % (ticker, str(only_now).lower()) , headers=self.headers)
+        if response.status_code != 200:
+            raise Exception("Error getting current earnings financials: ", response.text)
+        return response.json()
+    
+    def getEarningsEffect(self, ticker: str):
+        response = get(self.backendurl + '/data/earnings/effect?ticker=%s' % (ticker) , headers=self.headers)
+        if response.status_code != 200:
+            raise Exception("Error getting current earnings effects: ", response.text)
         return response.json()
 
 if __name__ == "__main__":
